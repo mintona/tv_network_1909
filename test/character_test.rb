522ff1d52161d3ecea22ba3e9a6988ae6538eaf8
Alison Vermeil <alison.minton@gmail.com>
2019-10-16 09:25:49 -0600

    Add character class with corresponding test file

---
+++ test/character_test.rb
@@ -0,0 +1,23 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/character'
+
+
+class CharacterTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end 
+
+  def test_it_exists
+    assert_instance_of Character, @kitt
+
+    assert_equal "KITT", @kitt.name
+    assert_equal "William Daniels", @kitt.actor
+    assert_equal 1000000, @kitt.salary
+  end
+
+
+  end
