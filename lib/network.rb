50539566dd901c8ddfc772abb9857c038e42786a
Alison Vermeil <alison.minton@gmail.com>
2019-10-16 09:56:03 -0600

    Remove unnecessary line of code from highest paid actor method

--- lib/network.rb
+++ lib/network.rb
@@ -12,8 +12,7 @@ class Network
   end
 
   def highest_paid_actor
-    all_characters = @shows.map { |show| show.characters}
-    all_characters = all_characters.flatten
+    all_characters = @shows.map { |show| show.characters}.flatten
     highest_paid_character = all_characters.max_by { |character| character.salary }
     highest_paid_character.actor
   end
